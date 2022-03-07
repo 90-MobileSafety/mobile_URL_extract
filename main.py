b35fca68377576bd2824c5bcac4ca46d90450e47
90-MobileSafety <62125142+90-MobileSafety@users.noreply.github.com>
2022-03-07 10:22:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from math import e
-import os, sys
-import re
-import hashlib
-import io
-import time
-import shutil
-#自行配置apktool和basksmali-2.4.0.jar路径
-basksmali = "path"
-apktool = "path"
+import os , re,io,shutil
+import sys
 
+bask = "./Tools/baksmali-2.4.0.jar"
 pattern = re.compile(r'(((file|gopher|news|nntp|telnet|http|ftp|https|ftps|sftp)://)|(www\.))+(([a-zA-Z0-9\._-]+\.[a-zA-Z]{2,6})|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(/[a-zA-Z0-9\&%_\./-~-]*)?')
-
+# '    const-string v1, "123.196.118.23"\n'
 pattern1 = re.compile(r'(25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]?\d)\.(25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]?\d)\.(25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]?\d)\.(25[0-5]|2[0-4]\d|[0-1]\d{2}|[1-9]?\d)')
+#pattern2 = re.compile(r'(?<=//|)(((\w)+\.)+\w+)(\:(\d+))')
+
+pattern2 = re.compile('(file|gopher|news|nntp|telnet|http|ftp|https|ftps|sftp)://([^/:]+)(:\d*)?')
 # 多重规则去匹配数据
 #dex 使用basksmali 进行反编译
 def Decompile_dex(filepath):
@@ -34,15 +31,24 @@ def dex_search(file):
     try:
         with io.open(file, 'r', encoding='utf-8') as smali:
             for line in smali.readlines():
-                line_num +=1
                 if 'android.com' in line:
                     continue
                 if pattern.search(line):
-                    ret_list.append('%s [line %d]: %s' % (file, line_num, line))
-                    
+                    newstr = pattern.search(line)
+                    ip = newstr[0]
+                    # newstr1 =pattern2.search(line)
+                    # ip1 =  newstr1[0]
+                    #pattern2.search()
+                    ret_list.append(' %s ' % (ip))
                 elif pattern1.search(line):
-                    ret_list.append('%s [line %d]: %s' % (file, line_num, line))    
-                    
+                    newstr = pattern1.search(line)
+                    ip = newstr[0]
+                    # newstr1 =pattern2.search(line)
+                    # ip1 =  newstr1[0]
+
+                    ret_list.append(' %s ' % (ip))    
+# # 存在 ip
+# 存在 http
             
     except Exception as e:
         print(e)
@@ -60,14 +66,15 @@ def dex_url_extract(filepath):
             savefile = dex_path+".md"
             for dirpath, _, filenames in os.walk(dex_path):
                     #跳过original目录
-                    if dirpath.endswith("original"):
+                    if dirpath.count("original") > 0:
                         continue
                     for filename in filenames:
                         #如果结尾不是smali活xml就跳出本次循环
-                        if (not filename.endswith('smali') and
-                            not filename.endswith('xml')):
-                            continue
+                        # if (not filename.endswith('smali') and
+                        #     not filename.endswith('xml') ):
+                        #     continue
                         file = os.path.join(dirpath, filename)
+                        print(dirpath)
                         ret = dex_search(file)
                         if len(ret) !=0:
                             #如果找到了url 就以追加的形式写到文件中
@@ -88,38 +95,93 @@ def dex_url_extract(filepath):
 def Decompile_apk(filepath):
     try:
 
-        str_cmd = apktool + " d "+ filepath +" -o "+tool(filepath)
+        str_cmd = "apktool" + " d "+ filepath +" -o "+tool(filepath)
         os.system(str_cmd)
         return tool(filepath)
     except Exception as e:
         return "decompile excepiton fail"
 
+def so_search(file):
+
+    try:
+    
+        result = os.popen("strings " +file +" | grep 'http'")
+        
+        dirResult = tool(file)
+        dirResult =dirResult +".md"
+        res = result.read()
+        f = io.open(dirResult,'a')
+        f.write(res)
+        f.close()
+        result =so_url_filter(dirResult)
+        #删除缓存文件
+        os.remove(dirResult)
+        return result
+    except Exception as e:
+        print(e)
+
+
+#因为so使用的是strings 所以要筛选一下  
+def so_url_filter(file):
+    print(file)
+    ret_list = []
+    try:
+        with io.open(file, 'rt', encoding='utf-8') as so:
+            result = so.readlines()
+            
+            for line in result:
+                print(line)
+                if 'android' in line:
+                    continue
+                if 'umeng' in line:
+                    continue
+                if 'adobe' in line:
+                    continue
+                if pattern.search(line):
+                    #print(line)
+                    ret_list.append('%s' % (line))
+                    
+                elif pattern1.search(line):
+                    ret_list.append('%s' %(line))    
+                    
+        return ret_list  
+    except Exception as e:
+        print(e)
+    
 #apk main
 def apk_url_extract(filepath):
-    apk_Decompile_path = Decompile_apk(filepath)
-    savefile = apk_Decompile_path+".md"   
-    for dirpath, _, filenames in os.walk(apk_Decompile_path):
-            #跳过original目录
-            if dirpath.endswith("original"):
-                continue
-            for filename in filenames:
-                #如果结尾不是smali活xml就跳出本次循环
-                if (not filename.endswith('smali') and
-                    not filename.endswith('xml')):
+    try:
+        apk_Decompile_path = Decompile_apk(filepath)
+        savefile = apk_Decompile_path+".md"   
+        for dirpath, _, filenames in os.walk(apk_Decompile_path):
+                #跳过original目录
+                if dirpath.count("original") > 0:
                     continue
-                file = os.path.join(dirpath, filename)
-                ret = dex_search(file)
-                if len(ret) !=0:
-                    #如果找到了url 就以追加的形式写到文件中
-                    #使用join 去除list并且添加换行
-                    cont_str = '\n'.join(ret)
-                    f = io.open(savefile,'a')
-                    f.write(cont_str)
-                
-    print("url提取结果保存到: "+savefile)
-    #删除缓存文件
-    shutil.rmtree(apk_Decompile_path)
+                for filename in filenames:
+                    
+                    #如果结尾不是smali或xml就跳出本次循环
+                    # if (not filename.endswith('smali') and
+                    #     not filename.endswith('xml')):
+                    #     continue
 
+                    file = os.path.join(dirpath, filename)
+                    if filename.endswith('so'):
+                        ret = so_search(file)
+                    else :
+                        ret = dex_search(file)
+                    if len(ret) !=0:
+                        #如果找到了url 就以追加的形式写到文件中
+                        #使用join 去除list并且添加换行
+                        cont_str = '\n'.join(ret)
+                        f = io.open(savefile,'a')
+                        f.write(cont_str)
+                        f.close()
+                    
+        print("url提取结果保存到: "+savefile)
+        #删除缓存文件
+        shutil.rmtree(apk_Decompile_path)
+    except Exception as e:
+            print(e)
 
 # dex目录遍历提取url
 # 注意需要遍历一个文件后 进行写入文件
@@ -137,13 +199,12 @@ def dirdex_url_extract(filepath):
                 dex_Decompile_path = Decompile_dex(Nfile)
                 for dirpath, _, filenames in os.walk(dex_Decompile_path):
                 #跳过original目录
-                    if dirpath.endswith("original"):
+                    if dirpath.count("original") > 0:
                         continue
                     for filename in filenames:
                         #如果结尾不是smali活xml就跳出本次循环
-                        if (not filename.endswith('smali') and
-                            not filename.endswith('xml')):
-                            continue
+                        # if filename.endswith('smali'):
+                        #     continue
                         file = os.path.join(dirpath, filename)
                         ret = dex_search(file)
                         if len(ret) !=0:
@@ -181,8 +242,8 @@ def ipa_url_filter(file):
     print(file)
     ret_list = []
     try:
-        with io.open(file, 'r', encoding='utf-8') as smali:
-            for line in smali.readlines():
+        with io.open(file, 'r', encoding='utf-8') as ipa:
+            for line in ipa.readlines():
                
                 if 'apple' in line:
                     continue
@@ -196,10 +257,10 @@ def ipa_url_filter(file):
                 elif pattern1.search(line):
                     ret_list.append('%s' %(line))    
                     
-            
+        return ret_list     
     except Exception as e:
         print(e)
-    return ret_list
+    
 def mainswitch(sw):
     
     if sw == "-a": #android
@@ -245,6 +306,7 @@ def tool(file):
 # 使用strings 读取可执行文件
 
 if __name__ == "__main__":
+    print("\t\t\t\t\t\t 使用前请阅读readme.md")
     print("\t\t\t\t\t\t 独立的 dex/apk web资产提取工具")
     print("\t\t\t\t\t\t 校验参数ing......[*]")
     if len(sys.argv) == 1 :
@@ -253,5 +315,4 @@ if __name__ == "__main__":
 
     sw = sys.argv[1]
     mainswitch(sw)
-
 
